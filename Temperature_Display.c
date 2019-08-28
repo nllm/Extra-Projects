d3f244cdc6e330bb4a11993675716eef549eac67
Nilli Martinez <52380654+nmsis@users.noreply.github.com>
2019-08-27 23:24:03 -0500

    Update Temperature_Display.c

--- Temperature_Display.c
+++ Temperature_Display.c
@@ -1,1 +1,94 @@
+/*
+This program uses read analog values from a LM35 sensor and show the values in a LCD Display
+*/
 
+// LCD module connections
+sbit LCD_RS at LATB4_bit;
+sbit LCD_EN at LATB5_bit;
+sbit LCD_D4 at LATB0_bit;
+sbit LCD_D5 at LATB1_bit;
+sbit LCD_D6 at LATB2_bit;
+sbit LCD_D7 at LATB3_bit;
+
+
+sbit LCD_RS_Direction at TRISB4_bit;
+sbit LCD_EN_Direction at TRISB5_bit;
+sbit LCD_D4_Direction at TRISB0_bit;
+sbit LCD_D5_Direction at TRISB1_bit;
+sbit LCD_D6_Direction at TRISB2_bit;
+sbit LCD_D7_Direction at TRISB3_bit;
+// End LCD module connections
+
+
+
+//  Set TEMP_RESOLUTION to the corresponding resolution:
+const unsigned short TEMP_RESOLUTION = 9;
+
+char *text = "000.0000";
+unsigned temp;
+
+void Display_Temperature(unsigned int temp2write) {
+  const unsigned short RES_SHIFT = TEMP_RESOLUTION - 8;
+  char temp_whole;
+  unsigned int temp_fraction;
+
+  // Check if temperature is negative
+  if (temp2write & 0x8000) {
+     text[0] = '-';
+     temp2write = ~temp2write + 1;
+     }
+
+  // Extract temp_whole
+  temp_whole = temp2write >> RES_SHIFT ;
+
+  // Convert temp_whole to characters
+  if (temp_whole/100)
+     text[0] = temp_whole/100  + 48;
+  else
+     text[0] = '0';
+
+  text[1] = (temp_whole/10)%10 + 48;             // Extract tens digit
+  text[2] =  temp_whole%10     + 48;             // Extract ones digit
+
+  // Extract temp_fraction and convert it to unsigned int
+  temp_fraction  = temp2write << (4-RES_SHIFT);
+  temp_fraction &= 0x000F;
+  temp_fraction *= 625;
+
+  // Convert temp_fraction to characters
+  text[4] =  temp_fraction/1000    + 48;         // Extract thousands digit
+  text[5] = (temp_fraction/100)%10 + 48;         // Extract hundreds digit
+  text[6] = (temp_fraction/10)%10  + 48;         // Extract tens digit
+  text[7] =  temp_fraction%10      + 48;         // Extract ones digit
+
+  // Print temperature on LCD
+  Lcd_Out(2, 5, text);
+}
+
+void main() {
+  ADCON1=0b00001111;
+  CMCON  |= 7;
+  UART1_Init(9600);
+  
+  Lcd_Init();                                    // Initialize LCD
+  Lcd_Cmd(_LCD_CLEAR);                           // Clear LCD
+  Lcd_Cmd(_LCD_CURSOR_OFF);                      // Turn cursor off
+  Lcd_Out(1, 1, " Temperature:   ");
+  // Print degree character, 'C' for Centigrades
+  Lcd_Chr(2,13,223);                             // Different LCD displays have different char code for degree
+                                                 // If you see greek alpha letter try typing 178 instead of 223
+
+  Lcd_Chr(2,14,'C');
+
+  //--- Main loop
+  do {
+    //--- Perform temperature reading
+    temp = (adc2 * 5.0 *100.0 ) / 1023.0;
+
+    //--- Format and display result on Lcd
+    Display_Temperature(temp/10);
+    UART1_Write_Text(temp);
+
+    Delay_ms(500);
+  } while (1);
+}
